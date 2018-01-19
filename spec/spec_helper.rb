c0f16ecb49a5d34b5be855cf3687f8baae6f63f4
James Hunt <ohthatjames@gmail.com>
2018-01-19 13:42:37 +0000

    Add check to ensure DbSanitiser is off by default.
    Now that our test suite turns the DbSanitiser on, we want to add a check
    to ensure that by default the SbSanitiser is off.
    
    The easiest way to achieve that is to raise an error at the top of the
    spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require "db_sanitiser"
 require 'active_record'
 require 'database_cleaner'
 
+raise "By default DbSanitiser should not be enabled" if DbSanitiser.enabled?
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
