3d13cbe642a5b157a8fb5c40b3fccd6cbdbbc03e
James Hunt <ohthatjames@gmail.com>
2018-01-19 09:37:07 +0000

    Wrap all of the specs in a describe block
    We want to add separate tests for validating without running the
    sanitisation, so this commit wraps all the specs in a block about
    actually running the sanitisation process.

--- spec/db_sanitiser/runner_spec.rb
+++ spec/db_sanitiser/runner_spec.rb
@@ -7,49 +7,49 @@ RSpec.describe DbSanitiser::Runner do
   class Hobby < ActiveRecord::Base
   end
 
-  describe 'sanitising tables' do
-    it 'can sanitise multiple tables' do
-      user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
-      user_id = user.id
-      hobby = Hobby.create(user_id: user_id, hobby: 'Saying yabba dabba doo')
-      hobby_id = hobby.id
-      described_class.new(fixture_file('all_sanitised.rb')).sanitise
-      expect(User.first.attributes).to eq("id" => user_id + 1000, "name" => 'Barney Rubble', "email" => 'barney.rubble@flintstones.com')
-      expect(Hobby.first.attributes).to eq("id" => user_id + 1000, "user_id" => hobby_id + 1000, "hobby" => 'surfing')
+  describe 'actually running the sanitiser' do
+    describe 'sanitising tables' do
+      it 'can sanitise multiple tables' do
+        user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
+        user_id = user.id
+        hobby = Hobby.create(user_id: user_id, hobby: 'Saying yabba dabba doo')
+        hobby_id = hobby.id
+        described_class.new(fixture_file('all_sanitised.rb')).sanitise
+        expect(User.first.attributes).to eq("id" => user_id + 1000, "name" => 'Barney Rubble', "email" => 'barney.rubble@flintstones.com')
+        expect(Hobby.first.attributes).to eq("id" => user_id + 1000, "user_id" => hobby_id + 1000, "hobby" => 'surfing')
+      end
+
+      it 'can sanitise a table depending on a query' do
+        user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
+        user2 = User.create!(name: 'Wilma Flintstone', email: 'wilma.flintstone@flintstones.com')
+
+        described_class.new(fixture_file('query_sanitised.rb')).sanitise
+        expect(User.first.name).to eq('Barney Rubble')
+        expect(User.last.name).to eq('Wilma Flintstone')
+      end
+
+      it 'can sanitise the same table multiple times' do
+        user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
+        user2 = User.create!(name: 'Wilma Flintstone', email: 'wilma.flintstone@flintstones.com')
+
+        described_class.new(fixture_file('query_sanitised_multiple.rb')).sanitise
+        expect(User.first.name).to eq('Barney Rubble')
+        expect(User.last.name).to eq('Betty Rubble')
+      end
     end
 
-    it 'can sanitise a table depending on a query' do
-      user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
-      user2 = User.create!(name: 'Wilma Flintstone', email: 'wilma.flintstone@flintstones.com')
+    describe 'deleting contents of tables' do
+      it 'deletes the contents of the table' do
+        user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
+        hobby = Hobby.create(user_id: 1, hobby: 'Saying yabba dabba doo')
 
-      described_class.new(fixture_file('query_sanitised.rb')).sanitise
-      expect(User.first.name).to eq('Barney Rubble')
-      expect(User.last.name).to eq('Wilma Flintstone')
+        described_class.new(fixture_file('delete_all.rb')).sanitise
+
+        expect(User.count).to eq(0)
+        expect(Hobby.count).to eq(0)
+      end
     end
 
-    it 'can sanitise the same table multiple times' do
-      user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
-      user2 = User.create!(name: 'Wilma Flintstone', email: 'wilma.flintstone@flintstones.com')
-
-      described_class.new(fixture_file('query_sanitised_multiple.rb')).sanitise
-      expect(User.first.name).to eq('Barney Rubble')
-      expect(User.last.name).to eq('Betty Rubble')
-    end
-  end
-
-  describe 'deleting contents of tables' do
-    it 'deletes the contents of the table' do
-      user = User.create!(name: 'Fred Flintstone', email: 'fred.flintstone@flintstones.com')
-      hobby = Hobby.create(user_id: 1, hobby: 'Saying yabba dabba doo')
-
-      described_class.new(fixture_file('delete_all.rb')).sanitise
-
-      expect(User.count).to eq(0)
-      expect(Hobby.count).to eq(0)
-    end
-  end
-
-  describe 'validating the schema' do
     it 'raises an error if there is an unknown column in the table being sanitised' do
       expect {
         described_class.new(fixture_file('missing_column.rb')).sanitise
