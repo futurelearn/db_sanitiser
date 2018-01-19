6fa5fe4b0c9ea68ee80852a118967ad735bdf8fc
James Hunt <ohthatjames@gmail.com>
2018-01-19 11:17:15 +0000

    Add convenience methods to call sanitise and validate
    Instead of having to instantiate a Runner, it makes sense to add methods
    to the DbSanitiser module, as these are the only two things that should
    be called by any client of this gem.

--- lib/db_sanitiser.rb
+++ lib/db_sanitiser.rb
@@ -2,5 +2,11 @@ require "db_sanitiser/version"
 require 'db_sanitiser/runner'
 
 module DbSanitiser
+  def self.sanitise(file_name)
+    Runner.new(file_name).sanitise
+  end
 
+  def self.validate(file_name)
+    Runner.new(file_name).validate
+  end
 end
